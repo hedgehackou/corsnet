61497a5e4ee868753cf6ecea94e31f3399aad7b5
fixable11 <fixable11@gmail.com>
2021-11-20 05:25:03 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@ set -e
 
 echo "Deploying application ..."
 
+git checkout master -f
+git pull origin master
+git fetch origin deploy
+git checkout deploy
+git merge master -m Merge
 cd back && make -f Makefile update && cd -;
 cd front && make -f Makefile update && cd -;
 :
