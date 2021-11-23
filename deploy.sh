4b02164d9cbd6353d6cb70b1014d6790c0fcd05b
fixable11 <fixable11@gmail.com>
2021-11-23 04:45:28 +0200

    Fix

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,9 @@ git checkout master -f
 git pull origin master
 git fetch origin deploy
 git checkout deploy
-git merge master -m Merge
+git reset --hard origin/deploy
+git merge master -m "Merge"
+git push origin deploy
 cd back && make -f Makefile update && cd -;
 cd front && make -f Makefile update && cd -;
 :
